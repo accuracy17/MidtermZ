2aaaf706043c097e0cb1d065944f58faa59ab8ff
accuracy17 <acucracy17@gmail.com>
2018-04-09 19:19:18 -0400

    MidtemMarch2018

--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -1,7 +1,9 @@
 package problems;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -11,20 +13,21 @@ public class DuplicateWord {
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-        String [] split_str = st.split(" "); // splitting string in word.
+        String rp = "Will you, William? Can't you, don't you, won't you, William?";
+
+        String [] split_rep = rp.split(" ");
         Map<String,Integer> myMap = new HashMap<String, Integer>();
-        for (String str : split_str ){
-            if(myMap.get(str)!=null){
-                myMap.put(str,myMap.get(str)+1);
+        for (String rep : split_rep ){
+            if(myMap.get(rep)!=null){
+                myMap.put(rep,myMap.get(rep)+1);
             }
             else {
-                myMap.put(str,1);
+                myMap.put(rep,1);
             }
         }
-        Set<String> str1= myMap.keySet();
-        for(String s : str1){
-            System.out.println(s+ " : Repeated "+myMap.get(s)+" time/s.");
+        Set<String> rep1= myMap.keySet();
+        for(String r : rep1){
+            System.out.println(r+ " : Repeated "+myMap.get(r)+" time/s.");
         }
     }
 }
