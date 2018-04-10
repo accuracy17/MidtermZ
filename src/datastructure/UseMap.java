84539935cb5ac9a61754015a192b09e28dc74cb1
accuracy17 <acucracy17@gmail.com>
2018-04-09 22:11:19 -0400

    MidtemMarch2018

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,25 +1,51 @@
 package datastructure;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class UseMap {
 
-	public static void main(String[] args) {
-		/*
-		 * Demonstrate how to use Map that includes storing and retrieving elements.
-		 * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
-		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 *
-		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-		 */
-			Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
-			myMap.put(1, 20);
-			myMap.put(2, 30);
-			myMap.put(3, 40);
+    public static void main(String[] args) {
+        /*
+         * Demonstrate how to use Map that includes storing and retrieving elements.
+         * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
+         * Use For Each loop and while loop with Iterator to retrieve data.
+         *
+         * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
+         */
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "Principal");
+        map.put(2, "Teacher");
+        map.put(3, "Security");
 
-			System.out.println(myMap.get(2));
-		}
+        System.out.println(map.get(1));
 
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
 
+        }
+
+        List<String> PrincipalDuties = new ArrayList<>();
+        PrincipalDuties.add("Lead the whole staff");
+        PrincipalDuties.add("Supervises");
+        PrincipalDuties.add("Annoucements");
+
+        List<String> TeacherDuties = new ArrayList<>();
+        TeacherDuties.add("Paper work");
+        TeacherDuties.add("Makes quizes");
+        TeacherDuties.add("Workshops");
+
+        List<String> SecurityDuties = new ArrayList<>();
+        SecurityDuties.add("Check cameras");
+        SecurityDuties.add("Greets");
+        SecurityDuties.add("Keep safe");
+
+        Map<String, List<String>> newMap = new LinkedHashMap<String, List<String>>();
+        newMap.put("Principal", PrincipalDuties);
+        newMap.put("Teacher", TeacherDuties);
+        newMap.put("Security", SecurityDuties);
+
+        for (Map.Entry entry : newMap.entrySet()) {
+            System.out.println(entry.getKey() + "  " + entry.getValue());
+        }
+    }
 }
